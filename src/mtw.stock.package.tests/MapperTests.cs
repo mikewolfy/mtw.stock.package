f5268ee87a6369cf2792afd430e6dcf25efa9cba
Michael Wolfgang <mike_wolfy@yahoo.com>
2020-05-04 09:37:29 -0400

    Added a mapper test

---
+++ src/mtw.stock.package.tests/MapperTests.cs
@@ -0,0 +1,46 @@
+﻿using Emptywolf.Stocks;
+using System;
+using Xunit;
+
+namespace mtw.stock.package.tests
+{
+    public class MapperTests
+    {
+        [Fact]
+        public void MapIexResponseToStockSuccess()
+        {
+            //setup
+            var iexResponse = new IexResponse
+            {
+                quote = new IexResponseQuote
+                {
+                    symbol = "AAPL",
+                    change = null,
+                    companyName = "Apple",
+                    latestPrice = 215.50M,
+                    open = 210.75M,
+                    peRatio = 30.45M,
+                    week52High = 250.10M,
+                    week52Low = 160.75M,
+                    changePercent = 0.20M,
+                    sector = "Technology"
+                }
+            };
+            var mapper = new Mapper();
+
+            //execute
+            var stock = mapper.MapIexResponseToStock(iexResponse);
+
+            //validate
+            Assert.Equal(iexResponse.quote.symbol, stock.Ticker);
+            Assert.Equal(iexResponse.quote.companyName, stock.Company);
+            Assert.Equal(iexResponse.quote.sector, stock.Sector);
+            Assert.Equal(iexResponse.quote.latestPrice, stock.Price);
+            Assert.Equal(iexResponse.quote.week52High, stock.Week52High);
+            Assert.Equal(iexResponse.quote.week52Low, stock.Week52Low);
+            Assert.False(stock.DailyPercentageChange == 0);
+            Assert.False(stock.DailyChange == 0);
+            Assert.True(DateTime.UtcNow.Subtract(stock.LastUpdated).TotalSeconds < 20);
+        }
+    }
+}
