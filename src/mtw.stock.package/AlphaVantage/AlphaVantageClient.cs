723c1cc9f8bdcff7413c30ae69cf1f1de14dcea4
Michael T Wolfgang <149036@carmax.com>
2021-02-05 16:50:55 -0500

    Added caching

--- src/mtw.stock.package/AlphaVantage/AlphaVantageClient.cs
+++ src/mtw.stock.package/AlphaVantage/AlphaVantageClient.cs
@@ -3,6 +3,7 @@ using Emptywolf.Stocks.Constants;
 using Microsoft.Extensions.Configuration;
 using Newtonsoft.Json;
 using System;
+using System.Collections.Generic;
 using System.Net.Http;
 using System.Threading.Tasks;
 
@@ -12,11 +13,14 @@ namespace Emptywolf.Stocks.AlphaVantage
     {
         private HttpClient _client;
         private string _key;
+        private Dictionary<string, CachedStock> _stockCache;
+        private readonly int _cacheTimeSeconds = 1200;
 
         public AlphaVantageClient(HttpClient client, IConfiguration config)
         {
             _key = config[AlphaVantageConfiguration.AlphaVantageApiKey];
             _client = client;
+            _stockCache = new Dictionary<string, CachedStock>();
         }
 
         public AlphaVantageClient(string key)
@@ -24,13 +28,40 @@ namespace Emptywolf.Stocks.AlphaVantage
             _key = key;
             _client = new HttpClient();
             _client.BaseAddress = new Uri("https://www.alphavantage.co/");
+            _stockCache = new Dictionary<string, CachedStock>();
         }
 
         public async Task<OverviewResponseDto> GetStockOverview(string ticker)
         {
+            if (_stockCache.TryGetValue(ticker.ToUpper(), out var cachedStock))
+            {
+                if (cachedStock != null && DateTime.UtcNow.Subtract(cachedStock.LastUpdated).TotalSeconds < _cacheTimeSeconds)
+                {
+                    return cachedStock.Dto;
+                }
+            }
+
             HttpResponseMessage task = await _client.GetAsync($"/query?function=OVERVIEW&symbol={ticker}&apikey={_key}");
             string jsonString = await task.Content.ReadAsStringAsync();
-            return JsonConvert.DeserializeObject<OverviewResponseDto>(jsonString);
+            var stockDto = JsonConvert.DeserializeObject<OverviewResponseDto>(jsonString);
+
+            var newCache = new CachedStock { Dto = stockDto, LastUpdated = DateTime.Now };
+            if (_stockCache.ContainsKey(ticker.ToUpper()))
+            {
+                _stockCache[ticker.ToUpper()] = newCache;
+            }
+            else
+            {
+                _stockCache.Add(ticker.ToUpper(), newCache);
+            }
+
+            return stockDto;
+        }
+
+        private class CachedStock
+        {
+            public OverviewResponseDto Dto { get; set; }
+            public DateTime LastUpdated { get; set; }
         }
     }
 }
