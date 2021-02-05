96eb436aaad3818852acfaee03eb4b79e4b49158
Michael T Wolfgang <149036@carmax.com>
2021-02-05 08:53:54 -0500

    Added basic client and model

---
+++ src/mtw.stock.package/AlphaVantage/AlphaVantageClient.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Emptywolf.Stocks.AlphaVantage
+{
+    public class AlphaVantageClient
+    {
+        public AlphaVantageClient(HttpClient client)
+        {
+
+        }
+    }
+}
